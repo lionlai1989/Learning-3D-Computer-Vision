bc2baf91ad851dc975a43121047e4b2b55789330
lai <lionlai1989@gmail.com>
2024-03-01 08:19:30 +0100

    Update losses.py

--- Supervised_Single_View_to_3D_Objects/losses.py
+++ Supervised_Single_View_to_3D_Objects/losses.py
@@ -5,7 +5,7 @@ from pytorch3d.ops.knn import knn_points
 
 def voxel_loss(voxel_src, voxel_tgt):
     # voxel_src, voxel_tgt: batch x height x width x depth
-    # Each value in voxel grid is either 1 or 0, meaning it occupies this voxel or not.
+    # Each value in voxel_tgt is either 1 or 0, meaning it occupies this voxel or not.
     # This is called binary voxel grid.
     #
     # Explain the differences between:
@@ -13,6 +13,8 @@ def voxel_loss(voxel_src, voxel_tgt):
     # `torch.nn.functional.binary_cross_entropy_with_logits` and `torch.nn.BCEWithLogitsLoss`
     #
     # TL;DR: Use BCEWithLogitsLoss for numerical stability.
+    # Read https://github.com/pytorch/pytorch/issues/751
+    # NOTE: Internally `BCEWithLogitsLoss` applies sigmoid function to input but NOT on target.
     loss = torch.nn.BCEWithLogitsLoss(reduction="mean")
     return loss(voxel_src, voxel_tgt)  # Returns a scalar Tensor.
 
@@ -28,18 +30,15 @@ def chamfer_loss(point_cloud_src, point_cloud_tgt):
     assert dim == 3 and dim == point_cloud_tgt.shape[2]
 
     k = 1  # the number of nearest neighbors
-    # knn_points returns K-Nearest neighbors on point clouds.
+    # knn_points returns the squared distances to the K-Nearest neighbors on point clouds.
     # src_dists, tgt_dists: (batch x n_points x k)
     src_dists, _, _ = knn_points(point_cloud_src, point_cloud_tgt, K=k)
     tgt_dists, _, _ = knn_points(point_cloud_tgt, point_cloud_src, K=k)
 
-    # Calculate the average distance. Thus, divide by n_points and batch_size.
-    # Returns a scalar Tensor.
-    return (torch.sum(src_dists) + torch.sum(tgt_dists)) / (n_points * batch_size)
+    return src_dists.mean() + tgt_dists.mean()  # Returns a scalar Tensor.
 
 
 def smoothness_loss(mesh_src):
     # https://pytorch3d.readthedocs.io/en/latest/_modules/pytorch3d/loss/mesh_laplacian_smoothing.html
     # It seems `mesh_laplacian_smoothing` already averages the loss by the size of the batch.
-    # Returns a scalar Tensor.
-    return mesh_laplacian_smoothing(mesh_src)
+    return mesh_laplacian_smoothing(mesh_src)  # Returns a scalar Tensor.
